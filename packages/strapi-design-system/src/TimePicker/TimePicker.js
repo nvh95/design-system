b2fd235da2f95b4f22f383e38bbe9fd8fa83e528
Gustav Hansen <gustav.hansen@strapi.io>
2022-02-28 09:57:36 +0100

    wip

--- packages/strapi-design-system/src/TimePicker/TimePicker.js
+++ packages/strapi-design-system/src/TimePicker/TimePicker.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Clock from '@strapi/icons/Clock';
 import styled from 'styled-components';
 import { sizes } from '../themes/sizes';
-import { Select, Option } from '../Select';
+import { Option } from '../Select';
 import { useId } from '../helpers/useId';
+
+import { Box } from '../Box';
+import { FieldInput } from '../Field';
+import { Popover } from '../Popover';
+import { SelectList } from '../Select/SelectList';
+import { KeyboardKeys } from '../helpers/keyboardKeys';
 
 const TimeIconWrapper = styled.div`
   display: flex;
@@ -19,6 +25,26 @@ const TimeIconWrapper = styled.div`
   }
 `;
 
+function generateSelectValues(step, hours) {
+  const hoursCount = 24;
+  const times = [];
+  let min = 0;
+
+  for (let i = 0; i < hoursCount; i++) {
+    min = 0;
+
+    if ((hours && i === hours) || !hours) {
+      while (min < 60) {
+        times.push(`${i < 10 ? '0' + i : i}:${min < 10 ? '0' + min : min}`);
+
+        min += step;
+      }
+    }
+  }
+
+  return times;
+}
+
 export const TimePicker = ({
   disabled,
   error,
@@ -26,6 +52,7 @@ export const TimePicker = ({
   id,
   onClear,
   onChange,
+  onError,
   value,
   clearLabel,
   label,
@@ -34,63 +61,129 @@ export const TimePicker = ({
   ...props
 }) => {
   const generatedId = useId('timepicker', id);
-  const hoursCount = 24;
-  const times = [];
-  let min = 0;
+  const containerRef = useRef(null);
+  const [popoverVisible, setPopoverVisible] = useState(false);
+  const [times, setTimes] = useState(generateSelectValues(step));
+  const [minutes, setMinutes] = useState(null);
+  const [hours, setHours] = useState(null);
 
-  for (let i = 0; i < hoursCount; i++) {
-    min = 0;
+  useEffect(() => {
+    const newTimes = generateSelectValues(step, hours, minutes);
+    setTimes(newTimes);
+  }, [hours]);
 
-    while (min < 60) {
-      times.push(`${i < 10 ? '0' + i : i}:${min < 10 ? '0' + min : min}`);
-      min += step;
+  function handleEscape() {
+    setPopoverVisible(false);
+  }
+
+  function onReachEnd() {
+    setPopoverVisible(false);
+  }
+
+  function handleOnBlur() {
+    if (hours && hours < 23 && minutes && minutes < 59) {
+      const paddedHours = `${hours}`;
+      const paddedMinutes = `${minutes}`;
+      const value = `${paddedHours}:${paddedMinutes}`;
+
+      onChange(value);
+    } else {
+      onError(new Error('Invalid value'));
     }
   }
 
-  // The time picker will select the closest value in the list.
-  // This is a temporary fix.
-  const getClosestValue = () => {
-    const valueHours = value.split(':')[0];
-    const valueMinutes = value.split(':')[1];
+  function handleOnChange(target) {
+    const { value } = target;
 
-    const hours = times.reduce((prev, curr) => {
-      const hours = curr.split(':')[0];
-      return Math.abs(hours - valueHours) < Math.abs(prev - valueHours) ? hours : prev;
-    }, times[0].split(':')[0]);
-    const minutes = times.reduce((prev, curr) => {
-      const minutes = curr.split(':')[1];
-      return Math.abs(minutes - valueMinutes) < Math.abs(prev - valueMinutes) ? minutes : prev;
-    }, times[0].split(':')[1]);
+    if (value.length === 0) {
+      onClear();
+    }
 
-    return `${hours}:${minutes}`;
-  };
+    const [rawHours, rawMinutes] = value.split(':');
+
+    const hours = parseInt(rawHours, 10) || 0;
+    const minutes = parseInt(rawMinutes, 10) || 0;
+
+    setMinutes(minutes);
+    setHours(hours);
+  }
+
+  function handleKeyDown(e) {
+    if (disabled) return;
+
+    switch (e.key) {
+      case KeyboardKeys.DOWN: {
+        e.preventDefault();
+        setPopoverVisible(true);
+        break;
+      }
+
+      case KeyboardKeys.UP: {
+        e.preventDefault();
+        setPopoverVisible(true);
+        break;
+      }
+
+      default:
+        break;
+    }
+  }
+
+  function handleOptionKeyDown(event, time) {
+    if (event.key === KeyboardKeys.SPACE || event.key === KeyboardKeys.ENTER) {
+      handleOptionSelect(time);
+    }
+  }
+
+  function handleOptionSelect(time) {
+    onChange(time);
+    setPopoverVisible(false);
+  }
 
   return (
-    <Select
-      id={generatedId}
-      label={label}
-      placeholder="--:--"
-      hint={hint}
-      onClear={onClear}
-      clearLabel={clearLabel}
-      error={error}
-      value={value ? getClosestValue() : null}
-      size={size}
-      onChange={onChange}
-      disabled={disabled}
-      startIcon={
-        <TimeIconWrapper>
-          <Clock />
-        </TimeIconWrapper>
-      }
-      {...props}
-    >
-      {times.map((time) => (
-        <Option value={time} key={time}>
-          {time}
-        </Option>
-      ))}
-    </Select>
+    <Box ref={containerRef}>
+      <FieldInput
+        id={generatedId}
+        label={label}
+        placeholder="--:--"
+        startAction={
+          <TimeIconWrapper>
+            <Clock />
+          </TimeIconWrapper>
+        }
+        value={value}
+        onFocus={() => setPopoverVisible(true)}
+        onChange={({ target }) => handleOnChange(target)}
+        onBlur={() => handleOnBlur()}
+        onKeyDown={handleKeyDown}
+        onClear={onClear}
+        error={error}
+      />
+
+      {popoverVisible && (
+        <Popover
+          source={containerRef}
+          spacing={4}
+          fullWidth
+          intersectionId={`time-picker-list-${generatedId}`}
+          onReachEnd={onReachEnd}
+        >
+          <SelectList selectId={generatedId} labelledBy={generatedId}>
+            {times.map((time) => (
+              <Option
+                value={time}
+                key={time}
+                onKeyDown={(event) => handleOptionKeyDown(event, time)}
+                onClick={() => handleOptionSelect(time)}
+                onEscape={() => setPopoverVisible(false)}
+              >
+                {time}
+              </Option>
+            ))}
+          </SelectList>
+        </Popover>
+      )}
+    </Box>
   );
 };
 
